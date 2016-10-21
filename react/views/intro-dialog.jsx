f062d9587c943ef5fd5f4b7a70d496ebc9f8cdfe
Christian Budde Christensen <budde377@gmail.com>
2016-10-21 23:18:14 +0200

    feat: Add sdk

--- react/views/intro-dialog.jsx
+++ react/views/intro-dialog.jsx
@@ -1,23 +1,56 @@
 const React = require('react')
+const { Sdk } = require('laundree-sdk')
+const config = require('config')
 
 class IntroDialog extends React.Component {
 
+  constructor (props) {
+    super(props)
+    this.sdk = new Sdk(config.get('laundree.address'))
+    this.state = {}
+  }
+
   get step () {
+    if (this.state.user) return 3
     if (this.props.cardId) return 2
     return 1
   }
 
+  componentWillReceiveProps ({ cardId }) {
+    if (this.props.cardId) return
+    const email = config.users[ cardId ]
+    if (!email) return
+    this.sdk.user
+      .fromEmail(email)
+      .then(({ id }) => this.sdk.user(id).get())
+      .then(user => this.setState({ user, photo: user.photo }))
+  }
+
+  renderStep () {
+    switch (this.step) {
+      case 1:
+        return <div className='step1'>
+          <h1>Please swipe your laundree card!</h1>
+        </div>
+      case 2:
+        return <div className='step2'>
+          <h1>Got it... Signing you in!</h1>
+        </div>
+      case 3:
+        return <div className='step3'>
+          <h1>Hi {this.state.user.name.givenName}!</h1>
+        </div>
+    }
+  }
+
   render () {
-    return <main id='IntroDialog' className={`step${this.step}`}>
-      <svg>
+    return <main id='IntroDialog'>
+      {this.state.photo
+        ? <img src={this.state.photo}/>
+        : <svg>
         <use xlinkHref='#IdCard'/>
-      </svg>
-      <div className='step1'>
-        <h1>Please swipe your laundree card!</h1>
-      </div>
-      <div className='step2'>
-        <h1>Got it... Signing you in!</h1>
-      </div>
+      </svg> }
+      {this.renderStep()}
     </main>
   }
 }
